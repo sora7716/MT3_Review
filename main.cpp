e63955c1a06e4abd744ac910c771794f6c47b2e8
sora <147121795+sora7716@users.noreply.github.com>
2025-07-25 14:03:59 +0900

    00_03を作成中

--- main.cpp
+++ main.cpp
@@ -13,29 +13,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Matrix4x4 m1 = {
-		3.2f,0.7f,9.6f,4.4f,
-		5.5f,1.3f,7.8f,2.1f,
-		6.9f,8.0f,2.6f,1.0f,
-		0.5f,7.2f,5.1f,3.3f
-	};
-
-	Matrix4x4 m2 = {
-		4.1f,6.5f,3.3f,2.2f,
-		8.8f,0.6f,9.9f,7.7f,
-		1.1f,5.5f,6.6f,0.0f,
-		3.3f,9.9f,8.8f,2.2f
-	};
-
-	Matrix4x4 add = {};
-	Matrix4x4 multiply = {};
-	Matrix4x4 sub = {};
-	Matrix4x4 inverse1 = {};
-	Matrix4x4 inverse2 = {};
-	Matrix4x4  transpose1 = {};
-	Matrix4x4  transpose2 = {};
-	Matrix4x4  identity = {};
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -48,14 +25,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓更新処理ここから
 		///
-		add = m1 + m2;
-		multiply = m1 * m2;
-		sub = m1 - m2;
-		inverse1 = ~m1;
-		inverse2 = ~m2;
-		transpose1 = m1.Transpose();
-		transpose2 = m2.Transpose();
-		identity = Matrix4x4::Indentity4x4();
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -63,14 +33,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(0, 0, add, "Add");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(0, ScreenPrintf::kRowHeight * 5, sub, "Subtract");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(0, ScreenPrintf::kRowHeight * 5 * 2, multiply, "Multiply");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(0, ScreenPrintf::kRowHeight * 5 * 3, inverse1, "inverse4");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(0, ScreenPrintf::kRowHeight * 5 * 4, inverse2, "inverse2");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(ScreenPrintf::kColumnWidth * 5, 0, transpose1, "transpose1");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(ScreenPrintf::kColumnWidth * 5, ScreenPrintf::kRowHeight * 5, transpose2, "transpose2");
-		ScreenPrintf::GetInstance()->MatrixScreenPrintf(ScreenPrintf::kColumnWidth * 5, ScreenPrintf::kRowHeight * 5 * 2, identity, "identity");
+		
 		///
 		/// ↑描画処理ここまで
 		///
